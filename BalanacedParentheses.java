70412b92020bb8f9bb2eea3a895191b1744dd782
sudheerchowdary <sudheer.chow1077@gmail.com>
2016-10-21 22:09:59 -0400

    Balnaced Parentheses POC

---
+++ BalanacedParentheses.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * Created by Sudheer Babu Gutha.
+ */
+public class BalanacedParentheses {
+
+    static Stack<Character> charStack = new Stack<Character>();
+
+    public static boolean validateParentheses(String input) {
+        if (input != null && !input.isEmpty()) {
+            for (int i = 0; i < input.length(); i++) {
+                char c = input.charAt(i);
+                if (c == '{' || c == '[' || c == '(') {
+                    charStack.push(c);
+                } else if (c == '}' || c == ']' || c == ')') {
+                    if (charStack.isEmpty())
+                        return false;
+                    switch (c) {
+                        case '}':
+                            if (charStack.pop() != '{')
+                                return false;
+                            break;
+                        case ']':
+                            if (charStack.pop() != '[')
+                                return false;
+                            break;
+                        case ')':
+                            if (charStack.pop() != '(')
+                                return false;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+        if (charStack.isEmpty())
+            return true;
+        return false;
+
+    }
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the input : ");
+        System.out.println(validateParentheses(in.next()));
+    }
+
+    /**
+     * Input: {}()[]
+     * Output: True
+     *
+     * Input: ({[]})
+     * Output: True
+     *
+     * Input: {})
+     * Output: false
+     */
+}
